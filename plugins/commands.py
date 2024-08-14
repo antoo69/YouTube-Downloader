14b6ec78f9e224758f7f1dac480edc06e8a7da9f
antoo69 <antobarry69@gmail.com>
2024-08-14 11:34:11 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,8 +21,8 @@ async def start(bot, cmd):
 					InlineKeyboardButton("⚔About", callback_data='about_cb')
 				],
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/kinu6'),
-					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/TMWAD")
+					InlineKeyboardButton("🕵️Developer", url='https://t.me/fsyrl'),
+					InlineKeyboardButton("💫Channel", url="https://t.me/filmlk21kw")
 				]
 			]
 		)
@@ -41,8 +41,8 @@ async def help(bot, cmd):
 					InlineKeyboardButton("⚡Back", callback_data='start_cb')
 				],
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/kinu6'),
-					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/TMWAD")
+					InlineKeyboardButton("🕵️Developer", url='https://t.me/fsyrl'),
+					InlineKeyboardButton("💫Channel", url="https://t.me/filmlk21kw")
 				]
 			]
 		)
@@ -60,8 +60,8 @@ async def about(bot, cmd):
 					InlineKeyboardButton("⚡Back", callback_data='start_cb')
 				],
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/kinu6'),
-					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/TMWAD")
+					InlineKeyboardButton("🕵️Developer", url='https://t.me/fsyrl'),
+					InlineKeyboardButton("💫Channel", url="https://t.me/filmlk21kw")
 				]
 			]
 		)
